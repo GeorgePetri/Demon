4f58baa86d439b753f31af2da22bc2eb18eaf00d
George Petri <jaxartez@gmail.com>
2019-01-12 14:01:17 +0200

    add StaticBeforeTarget

---
+++ AssemblyToProcess/StaticBeforeTarget.cs
@@ -0,0 +1,7 @@
+namespace AssemblyToProcess
+{
+    public class StaticBeforeTarget
+    {
+        public string Target(int parameter) => parameter.ToString();
+    }
+}
--- AssemblyToProcess/ThrowExceptionAspect.cs
+++ AssemblyToProcess/ThrowExceptionAspect.cs
@@ -6,7 +6,7 @@ namespace AssemblyToProcess
     [Aspect]
     public class ThrowExceptionAspect
     {
-        [Before("Todo")]
+        [Before("execution(AssemblyToProcess.StaticBeforeTarget.Target)")]
         public static void Advice() =>
             throw new ApplicationException("Exception from an aspect");
     }
