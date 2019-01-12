9702d031ae90d2329f3fecb8ee0774f3ed16ed88
George Petri <jaxartez@gmail.com>
2019-01-12 14:47:38 +0200

    impl GetBeforeAdvice

--- Demon.Fody/ModuleWeaver.cs
+++ Demon.Fody/ModuleWeaver.cs
@@ -10,7 +10,8 @@ namespace Demon.Fody
         public override void Execute()
         {
             var aspects = GetAspects();
-            
+            var advice = aspects.SelectMany(GetBeforeAdvice).ToList();
+
             var objectType = FindType("System.Object");
             var objectImport = ModuleDefinition.ImportReference(objectType);
             ModuleDefinition.Types.Add(new TypeDefinition("MyNamespace", "MyType", TypeAttributes.Public,
@@ -27,6 +28,12 @@ namespace Demon.Fody
             ModuleDefinition.Types
                 .Where(t => t.CustomAttributes
                     .Any(a => a.AttributeType.FullName == "Demon.Aspect.AspectAttribute"));
+
+        private static IEnumerable<(MethodDefinition method, string pointCutExpression)> GetBeforeAdvice(TypeDefinition aspect) =>
+            from method in aspect.Methods
+            from attribute in method.CustomAttributes
+            where attribute.AttributeType.FullName == "Demon.Aspect.BeforeAttribute"
+            select (method, (string) attribute.ConstructorArguments[0].Value);
 
         public override bool ShouldCleanReference { get; } = false;
     }
