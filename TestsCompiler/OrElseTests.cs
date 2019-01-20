2bf3fa28c08a6a8ecb53bb11384ce8ec89dfcc90
George Petri <jaxartez@gmail.com>
2019-01-20 17:17:38 +0200

    impl OrElseTests

---
+++ TestsCompiler/OrElseTests.cs
@@ -0,0 +1,42 @@
+using Demon.Fody.PointcutExpression;
+using Fody;
+using Mono.Cecil;
+using TestsCompiler.Helpers;
+using Xunit;
+
+namespace TestsCompiler
+{
+    public class OrElseTests
+    {
+        readonly ModuleDefinition _module = ModuleDefinition.ReadModule("TestDataForCompiler.dll");
+
+        [Fact]
+        public void Within()
+        {
+            //arrange
+            const string expression = @"Within(TestDataForCompiler.Services.**) Within(**.Post) ||";
+
+            var compiler = new Compiler(expression);
+
+            //act
+            var func = compiler.Compile();
+
+            var result = _module.FilterModule(func);
+
+            //assert   
+            Assert.Equal(4, result.Count);
+        }
+
+        [Theory]
+        [InlineData(@"|| Within(TestDataForCompiler.Controllers.**) Within(**.Get)")]
+        [InlineData(@"Within(TestDataForCompiler.Controllers.**) || Within(**.Get)")]
+        public void Throws_IfIsFirstOrSecondToken(string expression)
+        {
+            //arrange
+            var compiler = new Compiler(expression);
+
+            //assert   
+            Assert.Throws<WeavingException>(() => compiler.Compile());
+        }
+    }
+}
