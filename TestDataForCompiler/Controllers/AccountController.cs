b426c76ba880072e8e45508b618275dcc4dac338
George Petri <jaxartez@gmail.com>
2019-01-20 17:02:14 +0200

    add controllers

---
+++ TestDataForCompiler/Controllers/AccountController.cs
@@ -0,0 +1,11 @@
+namespace TestDataForCompiler.Controllers
+{
+    public class AccountController
+    {
+        public string Get() => "Account1";
+
+        public void Post(string user)
+        {
+        }
+    }
+}
---
+++ TestDataForCompiler/Controllers/UserController.cs
@@ -0,0 +1,11 @@
+namespace TestDataForCompiler.Controllers
+{
+    public class UserController
+    {
+        public string Get() => "User1";
+
+        public void Post(string user)
+        {
+        }
+    }
+}
