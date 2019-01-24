bd54364a89982805df3f9365579dec65bc79084e
George Petri <jaxartez@gmail.com>
2019-01-24 20:04:36 +0200

    add more constructors to InstanceBeforeTarget

--- AssemblyToProcess/BeforeAdvice/Instance/InstanceBeforeTarget.cs
+++ AssemblyToProcess/BeforeAdvice/Instance/InstanceBeforeTarget.cs
@@ -7,6 +7,19 @@ namespace AssemblyToProcess.BeforeAdvice.Instance
         public InstanceBeforeTarget(string dependency)
         {
             _dependency = dependency;
+        }  
+        
+        public InstanceBeforeTarget(string dependency, int ignored)
+        {
+            _dependency = dependency;
+        }
+        
+        public InstanceBeforeTarget(string dependency, bool ignored) : this(dependency)
+        {
+        }
+
+        public InstanceBeforeTarget() : this(null)
+        {
         }
 
         public string Target(int parameter) => parameter + _dependency;
