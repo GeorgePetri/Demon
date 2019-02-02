de58516ee013b796398386d668f7f7712bc4d7d7
George Petri <jaxartez@gmail.com>
2019-02-02 13:27:00 +0200

    add assemblies parameter to task

--- DemonWeaver/Demon.cs
+++ DemonWeaver/Demon.cs
@@ -1,15 +1,22 @@
+using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 
 namespace DemonWeaver
 {
     public class Demon : Task
     {
+        [Required]
+        public string[] Assemblies { get; set; }
+
         public override bool Execute()
         {
-
 //            var module = ModuleDefinition.ReadModule(@".\bin\Debug\netcoreapp2.1\AssemblyToProcess.dll");
 
-//            Log.LogWarning(module.Name);
+
+            foreach (var assembly in Assemblies)
+            {
+                Log.LogWarning(assembly);
+            }
 
             return true;
         }
