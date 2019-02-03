8c9126b1d9c60ae8d906fb45874c2d8bfd0fd89c
George Petri <jaxartez@gmail.com>
2019-02-03 19:15:06 +0200

    add syntax concept for args

--- AssemblyToProcess/PointcutExpressions.cs
+++ AssemblyToProcess/PointcutExpressions.cs
@@ -2,13 +2,41 @@ using Demon.Aspect;
 
 namespace AssemblyToProcess
 {
-    //todo define a nicer syntax than public void methods with empty body
-    //todo replace with rpn, add more complex ones
+    //todo, define form for:
+    //todo args with generics
+    //todo return type
+    //todo sync/async/all public/nonpublic
+    //todo attributes, for of args and method
     [Aspect]
     public class PointcutExpressions
     {
         [Pointcut("Within(AssemblyToProcess.BeforeAdvice.Static.StaticBeforeTarget.Target)")]
         void StaticBefore()
+        {
+        }
+
+        [Pointcut("Args()")]
+        void ArgsEmpty()
+        {
+        }
+
+        [Pointcut("Args(i)")]
+        void ArgsWithOneBound(int i)
+        {
+        }
+
+        [Pointcut("Args(i,s)")]
+        void ArgsWithTwoBound(int i, string s)
+        {
+        }
+
+        [Pointcut("Args(i,*)")]
+        void ArgsWithOneBoundAndAnotherAny()
+        {
+        }
+
+        [Pointcut("Args(i,**)")]
+        void ArgsWithOneBoundAndAnyOfAnyType()
         {
         }
     }
