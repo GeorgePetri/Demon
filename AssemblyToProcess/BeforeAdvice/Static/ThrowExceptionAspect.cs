b926a062b3fa010868f329a68e89b9a22a82ab8e
George Petri <jaxartez@gmail.com>
2019-02-03 18:57:31 +0200

    test using a pointcut

--- AssemblyToProcess/BeforeAdvice/Static/ThrowExceptionAspect.cs
+++ AssemblyToProcess/BeforeAdvice/Static/ThrowExceptionAspect.cs
@@ -7,7 +7,7 @@ namespace AssemblyToProcess.BeforeAdvice.Static
     [Aspect]
     public class ThrowExceptionAspect
     {
-        [Before("Within(AssemblyToProcess.BeforeAdvice.Static.StaticBeforeTarget.Target)")]
+        [Before("StaticBefore()")]
         public static void Advice(TypeInfo t) =>
             throw new ApplicationException("Exception from an aspect");
     }
--- AssemblyToProcess/PointcutExpressions.cs
+++ AssemblyToProcess/PointcutExpressions.cs
@@ -4,41 +4,11 @@ namespace AssemblyToProcess
 {
     //todo define a nicer syntax than public void methods with empty body
     //todo replace with rpn, add more complex ones
-//    [Aspect]
+    [Aspect]
     public class PointcutExpressions
     {
-        [Pointcut("Within(AssemblyToProcess.Repositories.**)")]
-        public void Repositories()
-        {
-        }
-
-        [Pointcut("Execution(* **.Get*(**))")]
-        public void Get()
-        {
-        }
-        
-        [Pointcut("Repositories() && Get()")]
-        public void RepositoriesGet()
-        {
-        }
-        
-        [Pointcut("Repositories() && !Get()")]
-        public void RepositoriesNonGet()
-        {
-        }
-        
-        [Pointcut("Execution(* **(Int))")]
-        public void ExecutionIntParameter()
-        {
-        }
-        
-        [Pointcut("Execution(* **(Int,*))")]
-        public void ExecutionIntAndOneAnyParameter()
-        {
-        }
-        
-        [Pointcut("Execution(* **(Int,**))")]
-        public void ExecutionIntAndManyAnyParameter()
+        [Pointcut("Within(AssemblyToProcess.BeforeAdvice.Static.StaticBeforeTarget.Target)")]
+        void StaticBefore()
         {
         }
     }
