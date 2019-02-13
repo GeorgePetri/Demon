2294bc35a8656d222e9197280c76893a11b9a144
George Petri <jaxartez@gmail.com>
2019-02-13 22:56:06 +0200

    rename

--- Demon/JoinPoint/TypeJoinPoint.cs
+++ Demon/JoinPoint/TypeJoinPoint.cs
@@ -3,9 +3,9 @@ using System.Reflection;
 namespace Demon.JoinPoint
 {
     //todo add MethodInfo
-    public class StaticJoinPoint
+    public class TypeJoinPoint
     {
-        public StaticJoinPoint(TypeInfo declaringType) => DeclaringType = declaringType;
+        public TypeJoinPoint(TypeInfo declaringType) => DeclaringType = declaringType;
 
         public TypeInfo DeclaringType { get; }
     }
