4570c6ebf8079f529d692223cd5cadbb19357d26
George Petri <jaxartez@gmail.com>
2019-02-24 00:25:43 +0200

    add BeforeTests

---
+++ TestsWeaving/BeforeTests.cs
@@ -0,0 +1,11 @@
+namespace TestsWeaving
+{
+    //todo test static target, static aspect
+    //todo test static target, instance aspect
+    //todo test instance target, static aspect
+    //todo test instance target, instance aspect
+    //todo test composition of aspects in other class
+    public class BeforeTests
+    {
+    }
+}
