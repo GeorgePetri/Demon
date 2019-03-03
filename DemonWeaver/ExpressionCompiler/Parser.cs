9d3ee94308e22bdc86679f6671b30b47c516c2dc
George Petri <jaxartez@gmail.com>
2019-03-03 16:41:07 +0200

    wip start on parser

--- DemonWeaver/ExpressionCompiler/Parser.cs
+++ DemonWeaver/ExpressionCompiler/Parser.cs
@@ -1,21 +1,55 @@
 using System.Collections.Generic;
+using System.Linq;
+using DemonWeaver.ExpressionCompiler.Sym.Interface;
+using DemonWeaver.ExpressionCompiler.Token;
 using DemonWeaver.ExpressionCompiler.Token.Interface;
 
 namespace DemonWeaver.ExpressionCompiler
 {
-    //handle eof
+    //handle eof, nulls, etc
     public class Parser
     {
         readonly List<IToken> _tokens;
-        readonly Stack<IToken> _stack = new Stack<IToken>(); //make another type
+        readonly Stack<ISym> _stack = new Stack<ISym>();
 
         public Parser(List<IToken> tokens)
         {
             _tokens = tokens;
         }
 
-        public void Parse()
+        public Stack<ISym> Parse()
         {
+            var first = Pop();
+
+            if (first is LeftParenToken leftParenToken)
+                LeftParen(leftParenToken);
+            else
+                throw new WeavingException("Expression must start with \"(\".");
+
+            return _stack;
+        }
+
+        void LeftParen(LeftParenToken parenToken)
+        {
+            var first = Pop();
+            
+            if(first is RightParenToken)
+                throw new WeavingException("() error"); //todo nicer message
+
+            while (true)
+            {
+                
+            }
+            
+        }
+
+        IToken Pop()
+        {
+            var result = _tokens.First();
+            
+            _tokens.RemoveAt(0);
+
+            return result;
         }
     }
 }
