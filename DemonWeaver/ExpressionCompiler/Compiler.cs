a9dc9956e5aee160bbf840a34482e485dbcbc673
George Petri <jaxartez@gmail.com>
2019-03-06 21:47:16 +0200

    add Compile

--- DemonWeaver/ExpressionCompiler/Compiler.cs
+++ DemonWeaver/ExpressionCompiler/Compiler.cs
@@ -1,4 +1,6 @@
-using DemonWeaver.ExpressionCompiler.Data;
+using System;
+using Mono.Cecil;
+using Environment = DemonWeaver.ExpressionCompiler.Data.Environment;
 using PointcutExpression = DemonWeaver.PointcutExpressionCompiler.Data.PointcutExpression;
 
 namespace DemonWeaver.ExpressionCompiler
@@ -10,5 +12,13 @@ namespace DemonWeaver.ExpressionCompiler
 
         public Compiler(PointcutExpression expression, Environment environment) =>
             (_expression, _environment) = (expression, environment);
+
+        public static Func<MethodDefinition, bool> Compile(PointcutExpression expression, Environment environment) =>
+            new Compiler(expression, environment).Compile();
+
+        public Func<MethodDefinition, bool> Compile()
+        {
+            return default;
+        }
     }
 }
