7e4506e0f8b4ce81fbf9e4148fbca02e01865740
George Petri <jaxartez@gmail.com>
2019-03-10 16:03:08 +0200

    do todo

--- DemonWeaver/ExpressionCompiler/Token/SymbolToken.cs
+++ DemonWeaver/ExpressionCompiler/Token/SymbolToken.cs
@@ -3,7 +3,6 @@ using DemonWeaver.ExpressionCompiler.Token.Interface;
 namespace DemonWeaver.ExpressionCompiler.Token
 {
     //todo is name right?
-    //todo replace usages with strings
     public class SymbolToken : IToken
     {
         public SymbolToken(string value) => Value = value;
