e693229097c6cb036198ce0644c859e5debbc5ec
George Petri <jaxartez@gmail.com>
2019-03-17 21:48:10 +0200

    test ToKebabCase

--- TestsCompiler/Unit/RegexTests.cs
+++ TestsCompiler/Unit/RegexTests.cs
@@ -25,9 +25,9 @@ namespace TestsCompiler.Unit
         [InlineData("b.")]
         [InlineData("b1")]
         [InlineData("qwertyuiop-asdfghjkl-ZXCVBNM*123456789")]
-        void Valid_Symbols(string value) => 
-            Assert.Matches(SymbolRegex, value);  
-        
+        void Valid_Symbols(string value) =>
+            Assert.Matches(SymbolRegex, value);
+
         [Theory]
         [InlineData("-")]
         [InlineData("1")]
@@ -40,9 +40,9 @@ namespace TestsCompiler.Unit
         [InlineData("b%")]
         [InlineData("b^")]
         [InlineData("b&")]
-        void Invalid_Symbols(string value) => 
+        void Invalid_Symbols(string value) =>
             Assert.DoesNotMatch(SymbolRegex, value);
-        
+
         [Theory]
         [InlineData("@b")]
         [InlineData("@B")]
@@ -51,19 +51,31 @@ namespace TestsCompiler.Unit
         [InlineData("@.")]
         [InlineData("@1")]
         [InlineData("@qwertyuiop-asdfghjkl-ZXCVBNM*123456789")]
-        void Valid_Strings(string value) => 
-            Assert.Matches(StringsRegex, value);  
-                
+        void Valid_Strings(string value) =>
+            Assert.Matches(StringsRegex, value);
+
         [Theory]
-        [InlineData("b")]
         [InlineData("@`")]
         [InlineData("@@")]
         [InlineData("@#")]
         [InlineData("@$")]
+        [InlineData("b")]
         [InlineData("@%")]
         [InlineData("@^")]
         [InlineData("@&")]
-        void Invalid_Strings(string value) => 
+        void Invalid_Strings(string value) =>
             Assert.DoesNotMatch(StringsRegex, value);
+
+        [Theory]
+        [InlineData("func", "func")]
+        [InlineData("func", "Func")]
+        [InlineData("pascal-case", "PascalCase")]
+        [InlineData("camel-case", "camelCase")]
+        [InlineData("kebab-case", "kebab-case")]
+        [InlineData("weird-case", "Weird-case")]
+        [InlineData("snake_case", "snake_case")]
+        [InlineData("-starts-with-minus", "-startsWithMinus")]
+        void ToKebabCase_ReturnsCorrect(string expected, string parameter) =>
+            Assert.Equal(expected, ToKebabCase(parameter));
     }
 }
