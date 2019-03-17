a76754b789801f4f60a10fe59c77a826181bc3f2
George Petri <jaxartez@gmail.com>
2019-03-17 13:05:27 +0200

    delete assembly to process

--- Demon/Aspect/AspectAttribute.cs
+++ Demon/Aspect/AspectAttribute.cs
@@ -2,6 +2,11 @@ using System;
 
 namespace Demon.Aspect
 {
+    //todo, define form for:
+    //todo args with generics
+    //todo return type
+    //todo sync/async/all public/nonpublic
+    //todo attributes, for of args and method
     [AttributeUsage(AttributeTargets.Class)]
     public class AspectAttribute : Attribute
     {
