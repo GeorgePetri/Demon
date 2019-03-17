d48a7af82f45ad3ab644f2a4a6bb0ead310d69d8
George Petri <jaxartez@gmail.com>
2019-03-17 21:34:26 +0200

    test symbols

--- TestsCompiler/Unit/RegexTests.cs
+++ TestsCompiler/Unit/RegexTests.cs
@@ -12,15 +12,35 @@ namespace TestsCompiler.Unit
     {
         static readonly Regex SymbolRegex = (Regex) typeof(Lexer).GetField("SymbolRegex", BindingFlags.NonPublic | BindingFlags.Static).GetValue(null);
         static readonly Regex StringsRegex = (Regex) typeof(Lexer).GetField("StringRegex", BindingFlags.NonPublic | BindingFlags.Static).GetValue(null);
+
         static readonly Func<string, string> ToKebabCase = (Func<string, string>) typeof(AspectModelBuilder)
             .GetMethod("ToKebabCase", BindingFlags.NonPublic | BindingFlags.Static)
             .Let(m => Delegate.CreateDelegate(typeof(Func<string, string>), null, m));
 
-        [Fact]
-        void Valid_Strings()
-        {
- 
-            var x = 0;
-        }
+        [Theory]
+        [InlineData("b")]
+        [InlineData("B")]
+        [InlineData("*")]
+        [InlineData("b-")]
+        [InlineData("b.")]
+        [InlineData("b1")]
+        [InlineData("qwertyuiop-asdfghjkl-ZXCVBNM*123456789")]
+        void Valid_Symbols(string value) => 
+            Assert.Matches(SymbolRegex, value);  
+        
+        [Theory]
+        [InlineData("-")]
+        [InlineData("1")]
+        [InlineData(".")]
+        [InlineData("111**.")]
+        [InlineData("b`")]
+        [InlineData("b@")]
+        [InlineData("b#")]
+        [InlineData("b$")]
+        [InlineData("b%")]
+        [InlineData("b^")]
+        [InlineData("b&")]
+        void Invalid_Symbols(string value) => 
+            Assert.DoesNotMatch(SymbolRegex, value);
     }
 }
