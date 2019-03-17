af350287eb749854b03874176ce4b3993ab7542f
George Petri <jaxartez@gmail.com>
2019-03-17 19:17:28 +0200

    add return types

--- DemonWeaver/DemonTypes.cs
+++ DemonWeaver/DemonTypes.cs
@@ -6,21 +6,29 @@ namespace DemonWeaver
 {
     public class DemonTypes
     {
-        DemonTypes(MethodDefinition joinPointConstructor, TypeDefinition typeJoinPoint, MethodDefinition typeJoinPointConstructor)
+        DemonTypes(MethodDefinition joinPointConstructor, TypeDefinition typeJoinPoint, MethodDefinition typeJoinPointConstructor, ReturnTypes returns)
         {
             JoinPointConstructor = joinPointConstructor;
             TypeJoinPoint = typeJoinPoint;
             TypeJoinPointConstructor = typeJoinPointConstructor;
+            Returns = returns;
         }
 
         public MethodDefinition JoinPointConstructor { get; }
         public TypeDefinition TypeJoinPoint { get; }
         public MethodDefinition TypeJoinPointConstructor { get; }
+        public ReturnTypes Returns { get; }
 
+        //todo refac, kinda ugly
         public static DemonTypes FromModule(ModuleDefinition module)
         {
             TypeDefinition joinPoint = default;
             TypeDefinition typeJoinPoint = default;
+
+            TypeDefinition returnAny = default;
+            TypeDefinition returnGeneric = default;
+            TypeDefinition returnVoid = default;
+
             foreach (var type in module.Types)
             {
                 switch (type.FullName)
@@ -31,10 +39,40 @@ namespace DemonWeaver
                     case FullNames.JoinPoint:
                         joinPoint = type;
                         break;
+                    case FullNames.ReturnFullNames.Any:
+                        returnAny = type;
+                        break;
+                    case FullNames.ReturnFullNames.Generic:
+                        returnGeneric = type;
+                        break;
+                    case FullNames.ReturnFullNames.Void:
+                        returnVoid = type;
+                        break;
                 }
             }
 
-            return new DemonTypes(joinPoint.GetConstructors().First(), typeJoinPoint, typeJoinPoint.GetConstructors().First());
+            return new DemonTypes(
+                joinPoint.GetConstructors().First(),
+                typeJoinPoint,
+                typeJoinPoint.GetConstructors().First(),
+                new ReturnTypes(
+                    returnAny.GetConstructors().First(),
+                    returnGeneric.GetConstructors().First(),
+                    returnVoid.GetConstructors().First()));
+        }
+
+        public class ReturnTypes
+        {
+            public ReturnTypes(MethodDefinition anyConstructor, MethodDefinition genericConstructor, MethodDefinition voidConstructor)
+            {
+                AnyConstructor = anyConstructor;
+                GenericConstructor = genericConstructor;
+                VoidConstructor = voidConstructor;
+            }
+
+            public MethodDefinition AnyConstructor { get; }
+            public MethodDefinition GenericConstructor { get; }
+            public MethodDefinition VoidConstructor { get; }
         }
 
         public static class FullNames
@@ -46,6 +84,13 @@ namespace DemonWeaver
 
             public const string JoinPoint = "Demon.JoinPoint.JoinPoint`2";
             public const string TypeJoinPoint = "Demon.JoinPoint.TypeJoinPoint";
+
+            public static class ReturnFullNames
+            {
+                public const string Any = "Demon.JoinPoint.Return.ReturnAny";
+                public const string Generic = "Demon.JoinPoint.Return.Return`1";
+                public const string Void = "Demon.JoinPoint.Return.ReturnVoid";
+            }
         }
     }
 }
