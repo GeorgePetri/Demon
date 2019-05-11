92320af4247539dbf501ad9ce16eb3bd35f3d168
George Petri <jaxartez@gmail.com>
2019-05-11 16:28:25 +0300

    use stateful lambda impl for joinpoints

--- DemonWeaver/AroundMethodWeaver.cs
+++ DemonWeaver/AroundMethodWeaver.cs
@@ -59,8 +59,6 @@ namespace DemonWeaver
 
             var joinPoint = _target.Module.ImportReference(_demonTypes.JoinPoint.MakeGenericInstanceType(parameterGeneric, returnGeneric));
 
-            var (cachedDelegateTypeField, cachedDelegateField, delegateMethod) = CreateLambdaType(parameterGeneric, returnGeneric);
-
             var lambda = CreateLambda(parameterGeneric, returnGeneric);
 
             ClearBody();
@@ -71,7 +69,7 @@ namespace DemonWeaver
 
             InsertLoadParameters(parameterGeneric);
             InsertLoadReturn(returnGeneric);
-            InsertLambdaAndCreateJoinPoint(parameterGeneric, returnGeneric, cachedDelegateTypeField, cachedDelegateField, delegateMethod);
+            InsertLambdaAndCreateJoinPoint(parameterGeneric, returnGeneric, lambda);
             _emitter.Stloc_0();
             _emitter.Ldarg_0();
             _emitter.Ldfld(_adviceField);
@@ -80,103 +78,11 @@ namespace DemonWeaver
             InsertRetWithReturnValue(parameterGeneric, returnGeneric);
         }
 
-        //todo after rev enging a bit, stateful lambdas are simpler, and don't use the caching mechanism
-        //Investigate further and delete the code, and use only stateful lambda impl, for mvp  
         //todo idea: naming tool like https://github.com/dotnet/roslyn/blob/master/src/Compilers/CSharp/Portable/Symbols/Synthesized/GeneratedNames.cs?
         //todo hardcoded name, might be conflicts
         //todo clean this up
         //todo idea: store common primitives definitions such as type of void, object constructor
-        (FieldDefinition cachedDelegateTypeField, FieldDefinition cachedDelegateField, MethodDefinition delegateMethod)
-            CreateLambdaType(TypeReference parametersType, TypeReference returnType)
-        {
-            var type = new TypeDefinition(
-                "",
-                "<Demon<>c",
-                TypeAttributes.Public | TypeAttributes.NestedPublic | TypeAttributes.Sealed | TypeAttributes.Serializable | TypeAttributes.BeforeFieldInit,
-                _target.Module.TypeSystem.Object);
-
-            //todo move somewhere if used more
-            var compilerGeneratedAttributeConstructor = _target.Module.ImportReference(
-                typeof(CompilerGeneratedAttribute)
-                    .GetConstructors().First());
-
-            type.CustomAttributes.Add(new CustomAttribute(compilerGeneratedAttributeConstructor));
-
-            //todo do i need to import self?
-            var cachedDelegateTypeField = new FieldDefinition(
-                "<Demon<>9",
-                FieldAttributes.FamANDAssem | FieldAttributes.Family | FieldAttributes.Static | FieldAttributes.InitOnly,
-                type);
-
-            type.Fields.Add(cachedDelegateTypeField);
-
-            var ctor = new MethodDefinition(
-                ".ctor",
-                MethodAttributes.FamORAssem | MethodAttributes.Family | MethodAttributes.HideBySig | MethodAttributes.SpecialName | MethodAttributes.RTSpecialName,
-                _target.Module.TypeSystem.Void);
-
-            var ctorEmitter = ctor.Body.GetILProcessor().Let(EmitterFactory.GetAppend);
-
-            ctorEmitter.Ldarg_0();
-            ctorEmitter.Call(_target.Module.ImportReference(typeof(object).GetConstructors().First()));
-            ctorEmitter.Ret();
-
-            type.Methods.Add(ctor);
-
-            var cctor = new MethodDefinition(
-                ".cctor",
-                MethodAttributes.Private | MethodAttributes.Static | MethodAttributes.HideBySig | MethodAttributes.SpecialName | MethodAttributes.RTSpecialName,
-                _target.Module.TypeSystem.Void);
-
-            var cctorEmitter = cctor.Body.GetILProcessor().Let(EmitterFactory.GetAppend);
-
-            cctorEmitter.Newobj(ctor);
-            cctorEmitter.Stsfld(cachedDelegateTypeField);
-            cctorEmitter.Ret();
-
-            type.Methods.Add(cctor);
-
-            var cachedDelegateFieldType = _target.Module.ImportReference(typeof(Action<,>))
-                .MakeGenericInstanceType(parametersType, returnType);
-
-            var cachedDelegateField = new FieldDefinition(
-                "<Demon<>9__3_0",
-                FieldAttributes.FamANDAssem | FieldAttributes.Family | FieldAttributes.Static,
-                cachedDelegateFieldType);
-
-            type.Fields.Add(cachedDelegateField);
-
-            var delegateMethod = new MethodDefinition(
-                $"<Demon<{_target.Name}>b__3_0",
-                MethodAttributes.Private | MethodAttributes.FamANDAssem | MethodAttributes.HideBySig,
-                _target.Module.TypeSystem.Void);
-
-            var delegateMethodParametersParameter = new ParameterDefinition(
-                "parameters",
-                ParameterAttributes.None,
-                parametersType);
-
-            var delegateMethodReturnParameter = new ParameterDefinition(
-                "ret",
-                ParameterAttributes.None,
-                returnType);
-
-            delegateMethod.Parameters.Add(delegateMethodParametersParameter);
-            delegateMethod.Parameters.Add(delegateMethodReturnParameter);
-
-            var delegateMethodEmitter = delegateMethod.Body.GetILProcessor().Let(EmitterFactory.GetAppend);
-
-//todo add code from original method here
-            delegateMethodEmitter.Ret();
-
-            type.Methods.Add(delegateMethod);
-
-            _target.DeclaringType.NestedTypes.Add(type);
-
-            return (cachedDelegateTypeField, cachedDelegateField, delegateMethod);
-        }
-
-        MethodReference CreateLambda(TypeReference parametersType, TypeReference returnType)
+        MethodDefinition CreateLambda(TypeReference parametersType, TypeReference returnType)
         {
             var lambdaMethod = new MethodDefinition(
                 $"<Demon<{_target.Name}>b__6_0",
@@ -262,41 +168,6 @@ namespace DemonWeaver
             }
 
             _emitter.Newobj(_target.Module.ImportReference(constructor));
-        }
-
-        void InsertLambdaAndCreateJoinPoint(
-            TypeReference parametersType,
-            TypeReference returnType,
-            FieldDefinition cachedDelegateTypeField,
-            FieldDefinition cachedDelegateField,
-            MethodDefinition delegateMethod)
-        {
-            _emitter.Ldsfld(cachedDelegateField);
-            _emitter.Dup();
-
-            var joinPointConstructor = _target.Module.ImportReference(_demonTypes.JoinPointConstructor.MakeGeneric(parametersType, returnType));
-            var createJoinPointInstruction = _il.Create(OpCodes.Newobj, joinPointConstructor);
-
-            _emitter.Brtrue_S(createJoinPointInstruction);
-            _emitter.Pop();
-            _emitter.Ldsfld(cachedDelegateTypeField);
-            _emitter.Ldftn(delegateMethod);
-
-            //todo get rid of getting the methodrefs here
-            //todo MakeGenericInstanceType might not be needed
-            var actionConstructor = _target.Module.ImportReference(typeof(Action<,>))
-                .MakeGenericInstanceType(parametersType, returnType)
-                .Resolve()
-                .GetConstructors()
-                .First()
-                .MakeGeneric(parametersType, returnType)
-                .Let(_target.Module.ImportReference);
-
-            _emitter.Newobj(actionConstructor);
-            _emitter.Dup();
-            _emitter.Stsfld(cachedDelegateField);
-
-            _il.Append(createJoinPointInstruction);
         }
 
         void InsertLambdaAndCreateJoinPoint(TypeReference parametersType, TypeReference returnType, MethodDefinition lambdaMethod)
